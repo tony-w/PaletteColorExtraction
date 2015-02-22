b067d66446c8e5685b21ea89b5a929221936231f
Tony <tonyw@cs.utexas.edu>
2015-02-22 16:00:08 -0600

    Cards added to layout using some sample images in assets directory.

--- app/src/main/java/com/tonyw/sampleapps/palettecolorextraction/MainActivity.java
+++ app/src/main/java/com/tonyw/sampleapps/palettecolorextraction/MainActivity.java
@@ -1,9 +1,19 @@
 package com.tonyw.sampleapps.palettecolorextraction;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +22,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            addCards();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addCards() throws IOException {
+        ViewGroup mainView = (ViewGroup) findViewById(R.id.main_view);
+        LayoutInflater inflater = getLayoutInflater();
+        AssetManager assetManager = getAssets();
+        for (String assetName : assetManager.list("sample_images")) {
+            InputStream assetStream = assetManager.open("sample_images/" + assetName);
+            Bitmap bitmap = BitmapFactory.decodeStream(assetStream);
+            CardView cardView = (CardView) inflater.inflate(R.layout.card_layout, null);
+            ((ImageView) cardView.findViewById(R.id.card_image)).setImageBitmap(bitmap);
+            mainView.addView(cardView);
+        }
     }
 
 
