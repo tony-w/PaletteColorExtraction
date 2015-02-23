e96f79485e0c5b528466c6e1e748522f44e4cc6b
Tony <tonyw@cs.utexas.edu>
2015-02-22 19:25:57 -0600

    Extract and display prominent colors!

--- app/src/main/java/com/tonyw/sampleapps/palettecolorextraction/MainActivity.java
+++ app/src/main/java/com/tonyw/sampleapps/palettecolorextraction/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.CardView;
@@ -39,7 +41,7 @@ public class MainActivity extends Activity {
         for (String assetName : assetManager.list("sample_images")) {
             InputStream assetStream = assetManager.open("sample_images/" + assetName);
             Bitmap bitmap = BitmapFactory.decodeStream(assetStream);
-            CardView cardView = (CardView) inflater.inflate(R.layout.card_layout, null);
+            final CardView cardView = (CardView) inflater.inflate(R.layout.card_layout, null);
             ((ImageView) cardView.findViewById(R.id.card_image)).setImageBitmap(bitmap);
             if (numCardsAdded++ % 2 == 1 && mainView2 != null) {
                 mainView2.addView(cardView);
@@ -49,7 +51,26 @@ public class MainActivity extends Activity {
             // Extract prominent colors asynchronously and then update the card.
             Palette.generateAsync(bitmap, new Palette.PaletteAsyncListener() {
                 public void onGenerated(Palette palette) {
-                    // Do something with colors...
+                    GradientDrawable vibrant = (GradientDrawable)
+                            cardView.findViewById(R.id.vibrant).getBackground();
+                    vibrant.setColor(palette.getVibrantColor(Color.BLACK));
+                    GradientDrawable vibrantDark = (GradientDrawable)
+                            cardView.findViewById(R.id.vibrant_dark).getBackground();
+                    GradientDrawable vibrantLight = (GradientDrawable)
+                            cardView.findViewById(R.id.vibrant_light).getBackground();
+                    GradientDrawable muted = (GradientDrawable)
+                            cardView.findViewById(R.id.muted).getBackground();
+                    vibrant.setColor(palette.getVibrantColor(Color.BLACK));
+                    GradientDrawable mutedDark = (GradientDrawable)
+                            cardView.findViewById(R.id.muted_dark).getBackground();
+                    GradientDrawable mutedLight = (GradientDrawable)
+                            cardView.findViewById(R.id.muted_light).getBackground();
+                    vibrant.setColor(palette.getVibrantColor(Color.BLACK));
+                    vibrantDark.setColor(palette.getDarkVibrantColor(Color.BLACK));
+                    vibrantLight.setColor(palette.getLightVibrantColor(Color.BLACK));
+                    muted.setColor(palette.getMutedColor(Color.BLACK));
+                    mutedDark.setColor(palette.getDarkMutedColor(Color.BLACK));
+                    mutedLight.setColor(palette.getLightMutedColor(Color.BLACK));
                 }
             });
         }
